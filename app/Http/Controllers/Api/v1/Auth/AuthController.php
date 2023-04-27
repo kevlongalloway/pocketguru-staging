3f1f089e96ee4dc9f5c2877331eb2255a3f5e375
Kevlon Galloway <kevlongalloway1999m@gmail.com>
2023-04-27 23:29:07 +0000

    initial commit

---
+++ app/Http/Controllers/Api/v1/Auth/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Auth;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    /**
+ * Register a new user and issue a token.
+ *
+ * @param  Illuminate\Http\Request  $request
+ * @return Illuminate\Http\JsonResponse
+ */
+    public function register(Request $request)
+{
+    // Validate the incoming request data
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:8',
+    ]);
+    
+    // Create the new user record
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password),
+    ]);
+    
+    // Issue a token for the new user
+    $token = $user->createToken('auth_token')->plainTextToken;
+    
+    // Return the token in JSON format
+    return response()->json([
+        'access_token' => $token,
+        'token_type' => 'Bearer',
+    ]);
+}
+
+/**
+ * Log in a user and issue a token.
+ *
+ * @param  Illuminate\Http\Request  $request
+ * @return Illuminate\Http\JsonResponse
+ */
+public function login(Request $request)
+{
+    // Validate the incoming request data
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+    
+    // Attempt to authenticate the user
+    if (!Auth::attempt($request->only('email', 'password'))) {
+        return response()->json(['error' => 'Invalid credentials'], 401);
+    }
+    
+    // Get the authenticated user
+    $user = $request->user();
+    
+    // Issue a token for the user
+    $token = $user->createToken('auth_token')->plainTextToken;
+    
+    // Return the token in JSON format
+    return response()->json([
+        'access_token' => $token,
+        'token_type' => 'Bearer',
+    ]);
+}
+
+}
---
+++ database/migrations/2023_04_27_161149_create_preferences_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreferencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('preferences', function (Blueprint $table) {
+            $table->id();
+	    $table->integer('user_id');
+	    $table->integer('meditation_preference')->nullable();
+	    $table->integer('affirmation_preference');
+	    $table->integer('breathing_exercise_preference');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('preferences');
+    }
+}
