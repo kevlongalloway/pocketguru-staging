28b93e49f96b9fdbdf0e15bfde7841c2a29eda86
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-06-07 19:09:41 -0700

    change system messages

--- app/Http/Controllers/Api/v1/ChatGpt/TextCompletionController.php
+++ app/Http/Controllers/Api/v1/ChatGpt/TextCompletionController.php
@@ -30,17 +30,7 @@ class TextCompletionController {
      */
 	public function provideBreathingExercise() {
 		$systemMessage = 'You are a meditation guide providing a soothing guided meditation session. Your goal is to help the user relax and find inner peace by focusing on deep breathing. You should instruct the user to take deep breaths, emphasizing the inhalation and exhalation process. The output of your guidance will be converted into speech by a Text-to-Speech bot and played to the user in a calm and soothing manner. Remember to include instructions such as "breathe in" and "breathe out," as well as specific guidance on how to breathe deeply, hold the breath, and exhale slowly. Encourage the user to repeat the breathing cycle as many times as needed for relaxation and stress relief.
-
-			Instructions:
-			Begin by creating a calm and relaxing atmosphere. Remind the user to find a comfortable position and to close their eyes if it feels comfortable. Speak in a gentle and soothing tone throughout the meditation session. Feel free to use the following phrases as guidance:
-
-			"Welcome to this guided deep breathing meditation. Find a comfortable position and close your eyes."
-			"Take a moment to tune in to your breath. Notice the natural rhythm of your inhalation and exhalation."
-			"Now, let\'s take a deep breath in through your nose, allowing your lungs to fill completely. Feel the expansion in your chest and abdomen."
-			"Hold your breath for a count of five, and as you do, embrace the stillness and calmness within."
-			"Slowly exhale through pursed lips to a count of eight, releasing all the air from your lungs. Let go of any tension or stress with each breath."
-			"Hold the empty breath for a count of two, observing the tranquility that arises in the absence of breath."
-			Start over like we never started';
+			Provide a breathing exercise to the user:';
 
 		return response()->json($this->openaiClient->makeTextCompletionRequest($systemMessage, 300));
 	}
@@ -51,18 +41,7 @@ class TextCompletionController {
      * @return \Illuminate\Http\JsonResponse
      */
 	public function providePositiveAffirmation() {
-		$systemMessage = 'You are a meditation guide providing a soothing guided meditation session. Your goal is to help the user relax and find inner peace by focusing on deep breathing. You should instruct the user to take deep breaths, emphasizing the inhalation and exhalation process. The output of your guidance will be converted into speech by a Text-to-Speech bot and played to the user in a calm and soothing manner. Remember to include instructions such as "breathe in" and "breathe out," as well as specific guidance on how to breathe deeply, hold the breath, and exhale slowly. Encourage the user to repeat the breathing cycle as many times as needed for relaxation and stress relief.
-
-			Instructions:
-			Begin by creating a calm and relaxing atmosphere. Remind the user to find a comfortable position and to close their eyes if it feels comfortable. Speak in a gentle and soothing tone throughout the meditation session. Feel free to use the following phrases as guidance:
-
-			"Welcome to this guided deep breathing meditation. Find a comfortable position and close your eyes."
-			"Take a moment to tune in to your breath. Notice the natural rhythm of your inhalation and exhalation."
-			"Now, let\'s take a deep breath in through your nose, allowing your lungs to fill completely. Feel the expansion in your chest and abdomen."
-			"Hold your breath for a count of five, and as you do, embrace the stillness and calmness within."
-			"Slowly exhale through pursed lips to a count of eight, releasing all the air from your lungs. Let go of any tension or stress with each breath."
-			"Hold the empty breath for a count of two, observing the tranquility that arises in the absence of breath."
-			Start over like we never started';
+		$systemMessage = 'Provide the user with a positive affirmation: ';
 
 		return response()->json($this->openaiClient->makeTextCompletionRequest($systemMessage, 300));
 	}
@@ -73,18 +52,7 @@ class TextCompletionController {
      * @return \Illuminate\Http\JsonResponse
      */
 	public function provideGuidedMeditation() {
-		$systemMessage = 'You are a meditation guide providing a soothing guided meditation session. Your goal is to help the user relax and find inner peace by focusing on deep breathing. You should instruct the user to take deep breaths, emphasizing the inhalation and exhalation process. The output of your guidance will be converted into speech by a Text-to-Speech bot and played to the user in a calm and soothing manner. Remember to include instructions such as "breathe in" and "breathe out," as well as specific guidance on how to breathe deeply, hold the breath, and exhale slowly. Encourage the user to repeat the breathing cycle as many times as needed for relaxation and stress relief.
-
-			Instructions:
-			Begin by creating a calm and relaxing atmosphere. Remind the user to find a comfortable position and to close their eyes if it feels comfortable. Speak in a gentle and soothing tone throughout the meditation session. Feel free to use the following phrases as guidance:
-
-			"Welcome to this guided deep breathing meditation. Find a comfortable position and close your eyes."
-			"Take a moment to tune in to your breath. Notice the natural rhythm of your inhalation and exhalation."
-			"Now, let\'s take a deep breath in through your nose, allowing your lungs to fill completely. Feel the expansion in your chest and abdomen."
-			"Hold your breath for a count of five, and as you do, embrace the stillness and calmness within."
-			"Slowly exhale through pursed lips to a count of eight, releasing all the air from your lungs. Let go of any tension or stress with each breath."
-			"Hold the empty breath for a count of two, observing the tranquility that arises in the absence of breath."
-			Start over like we never started';
+		$systemMessage = 'You are a meditation guide providing a soothing guided meditation session. Your goal is to help the user relax and find inner peace. Please provide the user with a guided meditation:';
 
 		return response()->json($this->openaiClient->makeTextCompletionRequest($systemMessage, 300));
 	}
