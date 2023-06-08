00b9ce2b1eb4ad124e56ea80e31ab21d547c2498
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-06-07 20:48:01 -0700

    remove migration file

--- database/migrations/2023_04_27_152821_create_answers_table.php
+++ database/migrations/2023_04_27_152821_create_answers_table.php
@@ -15,6 +15,7 @@ class CreateAnswersTable extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->nullable();
             $table->timestamps();
         });
     }
