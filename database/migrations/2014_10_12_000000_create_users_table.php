42d740421129d253cf669dcb594bffe141e47490
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-06-08 17:45:31 -0700

    fix subscription_tier NOT NULL error on user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->foreignId('subscription_tier_id')->constrained('subscription_tiers')->default(1);
+            $table->foreignId('subscription_tier_id')->default(1)->constrained('subscription_tiers');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
