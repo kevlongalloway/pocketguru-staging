72e82e83ba8f4dc28eec5ba1b8c01cdcfa813534
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-06-09 23:31:41 -0700

    Add seeder to populate database with dummy questions and options

--- database/migrations/2023_04_27_152626_create_questions_table.php
+++ database/migrations/2023_04_27_152626_create_questions_table.php
@@ -15,6 +15,7 @@ class CreateQuestionsTable extends Migration
     {
         Schema::create('questions', function (Blueprint $table) {
             $table->id();
+            $table->string('question');
             $table->integer('question_type');
             $table->timestamps();
         });
