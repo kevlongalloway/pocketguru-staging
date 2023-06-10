d536c75d51006719aac033ad288db34710cba462
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-06-09 23:28:15 -0700

    Add seeder to populate database with dummy questions and options

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,95 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Question;
+use App\Models\Option;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeders.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Seed questions and options
+        //ONLY FOR TESTING REMOVE IN PRODUCTION
+        $this->seedQuestionsWithOptions();
+    }
+
+    /**
+     * Seed questions and options.
+     *
+     * @return void
+     */
+    private function seedQuestionsWithOptions()
+    {
+        // Array of dummy questions with their options
+        $questions = [
+            [
+                'question' => 'What is your favorite color?',
+                'question_type' => 1,
+                'options' => [
+                    'Red',
+                    'Blue',
+                    'Green',
+                    'Yellow',
+                ],
+            ],
+            [
+                'question' => 'What is your favorite animal?',
+                'question_type' => 1,
+                'options' => [
+                    'Dog',
+                    'Cat',
+                    'Elephant',
+                    'Lion',
+                ],
+            ],
+            [
+                'question' => 'What is your favorite animal?',
+                'question_type' => 1,
+                'options' => [
+                    'Dog',
+                    'Cat',
+                    'Elephant',
+                    'Lion',
+                ],
+            ],
+            [
+                'question' => 'What is your favorite animal?',
+                'question_type' => 1,
+                'options' => [
+                    'Dog',
+                    'Cat',
+                    'Elephant',
+                    'Lion',
+                ],
+            ],
+            [
+                'question' => 'What is your age?',
+                'question_type' => 2,
+                'options' => [],
+            ],
+        ];
+
+        foreach ($questions as $questionData) {
+            // Create the question
+            $question = Question::create([
+                'question' => $questionData['question'],
+                'question_type' => $questionData['question_type'],
+            ]);
+
+            if ($questionData['question_type'] === 1) {
+                // Create options for multiple choice questions
+                foreach ($questionData['options'] as $option) {
+                    Option::create([
+                        'question_id' => $question->id,
+                        'option' => $option,
+                    ]);
+                }
+            }
+        }
     }
 }
