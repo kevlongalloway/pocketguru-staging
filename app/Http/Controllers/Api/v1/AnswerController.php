29807c1b3607ea3265d119bf7432a7be9e709b77
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-06-16 17:43:30 -0700

    Fix AnswerController bugs

--- app/Http/Controllers/Api/v1/AnswerController.php
+++ app/Http/Controllers/Api/v1/AnswerController.php
@@ -22,22 +22,21 @@ class AnswerController extends Controller {
         $validatedData = $request->validate([
             'answers' => 'required|array',
             'answers.*.question_id' => 'required|integer',
-            'answers.*.answer_type' => 'required|integer',
-            'answers.*.option_id' => 'nullable|required_if:answers.*.answer_type,1|integer',
-            'answers.*.content' => 'nullable|required_if:answers.*.answer_type,2|string',
+            'answers.*.question_type' => 'required|integer',
+            'answers.*.option_id' => 'nullable|required_if:answers.*.question_type,1|integer',
+            'answers.*.content' => 'nullable|required_if:answers.*.question_type,2|string',
         ]);
 
         $user = Auth::user();
 
         foreach ($request->answers as $answerData) {
             $answer = new Answer([
-                'answer_type' => $answerData['answer_type'],
                 'question_id' => $answerData['question_id'],
             ]);
 
-            if ($answerData['answer_type'] === 1) {
+            if ($answerData['question_type'] === 1) {
                 $answer->option_id = $answerData['option_id'];
-            } elseif ($answerData['answer_type'] === 2) {
+            } elseif ($answerData['question_type'] === 2) {
                 $answer->content = $answerData['content'];
             }
 
