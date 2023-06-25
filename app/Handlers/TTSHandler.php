75c46351eea9b287619adeb8f1e8923ab94fd04c
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-06-25 13:51:20 -0700

    Update TTSHandler to use storage_path and generate access token

--- app/Handlers/TTSHandler.php
+++ app/Handlers/TTSHandler.php
@@ -13,17 +13,39 @@ class TTSHandler {
 		$this->parentUrl = env('TTS_PARENT_URL', 'projects/%s/locations/%s'); // Fetch the parent URL from the environment variable
 	}
 
-	private function getApiKey() {
-		$keyFilePath = storage_path('pg-tts-390208.json');
+    private function getAccessToken() {
+    $keyFilePath = storage_path('pg-tts-390208.json');
 
-		if (file_exists($keyFilePath)) {
-			$jsonKey = file_get_contents($keyFilePath);
-			$keyData = json_decode($jsonKey, true);
-			return $keyData['private_key'];
-		} else {
-			throw new \Exception('API key file not found.');
-		}
-	}
+    if (file_exists($keyFilePath)) {
+        $jsonKey = file_get_contents($keyFilePath);
+        $keyData = json_decode($jsonKey, true);
+        $privateKey = $keyData['private_key'];
+        $clientId = $keyData['client_id'];
+
+        $now = time();
+        $expiresIn = 3600; // Token expires in 1 hour
+        $payload = [
+            'iss' => $clientId,
+            'sub' => $clientId,
+            'aud' => 'https://oauth2.googleapis.com/token',
+            'iat' => $now,
+            'exp' => $now + $expiresIn,
+            'scope' => 'https://www.googleapis.com/auth/cloud-platform',
+        ];
+
+        $jwt = \Firebase\JWT\JWT::encode($payload, $privateKey, 'RS256');
+
+        $response = Http::asForm()->post('https://oauth2.googleapis.com/token', [
+            'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+            'assertion' => $jwt,
+        ]);
+
+        $accessToken = $response->json()['access_token'];
+
+        return $accessToken;
+    } else {
+        throw new \Exception('API key file not found.');
+    }
 
 	public function synthesizeAudio($input, $voiceName, $ssml = false, $outputFormat = 'MP3', $sampleRate = 48000) {
 		$project = env('TTS_PROJECT');
