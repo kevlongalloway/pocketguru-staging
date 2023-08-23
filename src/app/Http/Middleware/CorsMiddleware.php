4eb0ca32f623803aafb5b11b8cdd7b2eab4a8f3e
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-08-23 16:09:37 -0700

    Implement CORS Policy for Enhanced Security and Controlled Access

--- src/app/Http/Middleware/CorsMiddleware.php
+++ src/app/Http/Middleware/CorsMiddleware.php
@@ -4,17 +4,27 @@ namespace App\Http\Middleware;
 
 use Closure;
 
-class CorsMiddleware
-{
-    public function handle($request, Closure $next)
-    {
-        $response = $next($request);
+class CorsMiddleware {
+	public function handle($request, Closure $next) {
+		// List of allowed origins (your web app and website URLs)
+		$allowedOrigins = [
+			'https://demo.pocketguruai.com',
+		];
 
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization');
-        $response->header('Access-Control-Allow-Credentials', 'true');
+		$origin = $request->header('Origin');
 
-        return $response;
-    }
+		if (in_array($origin, $allowedOrigins)) {
+			$response = $next($request);
+
+			$response->header('Access-Control-Allow-Origin', $origin);
+			$response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+			$response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization');
+			$response->header('Access-Control-Allow-Credentials', 'true');
+
+			return $response;
+		}
+
+		// Return a response with an error message for unauthorized origins
+		return response()->json(['message' => 'Unauthorized origin'], 403);
+	}
 }
