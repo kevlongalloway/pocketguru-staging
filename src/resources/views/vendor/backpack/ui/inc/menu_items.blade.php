321d36b028b88bfc9267668b46928d999222ec88
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-08-23 10:34:51 -0700

    Revise admin menu for clarity and modern icons

--- src/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ src/resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -2,12 +2,13 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="las la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
 
 <x-backpack::menu-dropdown title="Resources" icon="las la-group">
+	<x-backpack::menu-dropdown-item title="Users" icon="las la-users" :link="backpack_url('user')" />
 	<x-backpack::menu-dropdown-item title="System messages" icon="las la-envelope" :link="backpack_url('system-message')" />
-	{{-- <x-backpack::menu-dropdown-item title="Answers" icon="las la-question-circle" :link="backpack_url('answer')" /> --}}
-	<x-backpack::menu-dropdown-item title="Options" icon="las la-cog" :link="backpack_url('option')" />
-	<x-backpack::menu-dropdown-item title="Preferences" icon="las la-sliders-h" :link="backpack_url('preference')" />
 	<x-backpack::menu-dropdown-item title="User Questions" icon="las la-question-circle" :link="backpack_url('question')" />
+	<x-backpack::menu-dropdown-item title="Options" icon="las la-cog" :link="backpack_url('option')" />
 	<x-backpack::menu-dropdown-item title="Services" icon="las la-wrench" :link="backpack_url('service')" />
 	<x-backpack::menu-dropdown-item title="Subscription tiers" icon="las la-layer-group" :link="backpack_url('subscription-tier')" />
-	<x-backpack::menu-item title="Users" icon="las la-users" :link="backpack_url('user')" />
+	{{-- <x-backpack::menu-dropdown-item title="Answers" icon="las la-question-circle" :link="backpack_url('answer')" /> --}}
+
+	<x-backpack::menu-dropdown-item title="Preferences" icon="las la-sliders-h" :link="backpack_url('preference')" />
 </x-backpack::menu-dropdown>
