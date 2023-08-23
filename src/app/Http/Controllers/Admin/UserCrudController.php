b5f82c4e3f76fd264a848bba04edb3d5bac133d7
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-08-23 11:23:02 -0700

    Implemented password hashing for user creation

--- src/app/Http/Controllers/Admin/UserCrudController.php
+++ src/app/Http/Controllers/Admin/UserCrudController.php
@@ -5,73 +5,84 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Requests\UserRequest;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use Illuminate\Support\Facades\Hash;
 
 /**
  * Class UserCrudController
  * @package App\Http\Controllers\Admin
  * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
  */
-class UserCrudController extends CrudController
-{
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+class UserCrudController extends CrudController {
+	use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+	use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+	use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+	use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+	use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
 
-    /**
-     * Configure the CrudPanel object. Apply settings to all operations.
-     * 
-     * @return void
-     */
-    public function setup()
-    {
-        CRUD::setModel(\App\Models\User::class);
-        CRUD::setRoute(config('backpack.base.route_prefix') . '/user');
-        CRUD::setEntityNameStrings('user', 'users');
-    }
+	/**
+	 * Configure the CrudPanel object. Apply settings to all operations.
+	 *
+	 * @return void
+	 */
+	public function setup() {
+		CRUD::setModel(\App\Models\User::class);
+		CRUD::setRoute(config('backpack.base.route_prefix') . '/user');
+		CRUD::setEntityNameStrings('user', 'users');
+	}
 
-    /**
-     * Define what happens when the List operation is loaded.
-     * 
-     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
-     * @return void
-     */
-    protected function setupListOperation()
-    {
-        CRUD::setFromDb(); // set columns from db columns.
+	/**
+	 * Define what happens when the List operation is loaded.
+	 *
+	 * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+	 * @return void
+	 */
+	protected function setupListOperation() {
+		CRUD::setFromDb(); // set columns from db columns.
 
-        /**
-         * Columns can be defined using the fluent syntax:
-         * - CRUD::column('price')->type('number');
-         */
-    }
+		/**
+		 * Columns can be defined using the fluent syntax:
+		 * - CRUD::column('price')->type('number');
+		 */
+	}
 
-    /**
-     * Define what happens when the Create operation is loaded.
-     * 
-     * @see https://backpackforlaravel.com/docs/crud-operation-create
-     * @return void
-     */
-    protected function setupCreateOperation()
-    {
-        CRUD::setValidation(UserRequest::class);
-        CRUD::setFromDb(); // set fields from db columns.
+	/**
+	 * Define what happens when the Create operation is loaded.
+	 *
+	 * @see https://backpackforlaravel.com/docs/crud-operation-create
+	 * @return void
+	 */
+	protected function setupCreateOperation() {
+		CRUD::setValidation(UserRequest::class);
 
-        /**
-         * Fields can be defined using the fluent syntax:
-         * - CRUD::field('price')->type('number');
-         */
-    }
+		CRUD::field('name')
+			->type('text')
+			->label('Name');
 
-    /**
-     * Define what happens when the Update operation is loaded.
-     * 
-     * @see https://backpackforlaravel.com/docs/crud-operation-update
-     * @return void
-     */
-    protected function setupUpdateOperation()
-    {
-        $this->setupCreateOperation();
-    }
+		CRUD::field('email')
+			->type('text')
+			->label('Email');
+
+		// Modify the password field to hash the password
+		CRUD::field('password')
+			->type('password')
+			->label('Password')
+			->beforeInsert(function ($value, $crud, $request, $field) {
+				return Hash::make($value); // Hash the password using Laravel's Hash::make() method
+			});
+
+		/**
+		 * Fields can be defined using the fluent syntax:
+		 * - CRUD::field('price')->type('number');
+		 */
+	}
+
+	/**
+	 * Define what happens when the Update operation is loaded.
+	 *
+	 * @see https://backpackforlaravel.com/docs/crud-operation-update
+	 * @return void
+	 */
+	protected function setupUpdateOperation() {
+		$this->setupCreateOperation();
+	}
 }
