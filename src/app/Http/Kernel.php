f1f3c00200723c8e185bc6213e549918cfe06352
kevlongalloway <kevlongalloway1999m@gmail.com>
2023-08-25 18:10:54 -0700

    move handleCors to global middleware

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel {
 		\Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
 		\App\Http\Middleware\TrimStrings::class,
 		\Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+		\Illuminate\Http\Middleware\HandleCors::class,
 
 	];
 
@@ -43,7 +44,6 @@ class Kernel extends HttpKernel {
 			'throttle:api',
 			\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
 			\Illuminate\Routing\Middleware\SubstituteBindings::class,
-			\App\Http\Middleware\CorsMiddleware::class,
 		],
 	];
 
